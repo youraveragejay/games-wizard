bfdb97b5b9777e59ba34932ae2cd2e8a8287a73a
youraveragejay <jcracknell08@gmail.com>
2022-11-29 22:22:51 +0000

    bot colour

--- data/config.js
+++ data/config.js
@@ -2,7 +2,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-    "guildId": "1033396516196470945",
-    "clientId": "998314312160514118",
-    "token": process.env.TOKEN,
-}
+  guildId: "1033396516196470945",
+  clientId: "998314312160514118",
+  token: process.env.TOKEN,
+  botColor: "#57c6b5",
+};
